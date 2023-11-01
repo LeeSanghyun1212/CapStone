4ee2e6be8895abc002ccb8c5e3fbb59c016deebf
LeeSanghyun1212 <gustkd7125@gmail.com>
2023-11-01 17:39:15 +0900

    commit message add

---
+++ app/src/main/java/com/example/test2/loadingActivity.kt
@@ -0,0 +1,26 @@
+package com.example.test2
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class loadingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+        // 로딩 화면 시작.
+        loadingStart()
+    }
+
+    private fun loadingStart() {
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(applicationContext, registerActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
---
+++ app/src/main/java/com/example/test2/registerActivity.kt
@@ -0,0 +1,15 @@
+package com.example.test2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class registerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_legister)
+
+    }
+}
