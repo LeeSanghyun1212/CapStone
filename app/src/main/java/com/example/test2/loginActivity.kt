06c64ee97b3c418a8438deac54f30d31451983d2
Ahn-JuHwan <ajuh1103@naver.com>
2023-11-08 19:37:36 +0900

    login

--- app/src/main/java/com/example/test2/loginActivity.kt
+++ app/src/main/java/com/example/test2/loginActivity.kt
@@ -19,7 +19,34 @@ class loginActivity : AppCompatActivity() {
         //파이어 베이스 초기화
         auth = FirebaseAuth.getInstance()
 
+        emailEditText = findViewById(R.id.Id)
+        passwordEditText = findViewById(R.id.Pw)
+        loginButton = findViewById(R.id.btn_login)
+
         //로그인 버튼 클릭시 이벤트 처리
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            // Firebase를 사용하여 로그인 시도
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // 로그인 성공
+                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+
+                        // 메인  부탁해요
+                        val intent = Intent(this, RegisterActivity::class.java)
+                        startActivity(intent)
+
+                        // 현재 액티비티를 종료합니다.
+                        finish()
+                    } else {
+                        // 로그인 실패
+                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
 
 
         //회원가입 버튼 클릭시 회원가입으로 이동
